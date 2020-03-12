b5ddadc603dba85a3c1ffdb0d87e7e25a1707e34
duchunyu <1005953428@qq.com>
2020-03-12 17:35:09 +0800

    iox-ui 增加 iox-tag 组件，修改transition引入字母大写

---
+++ src/components/iox-tag/iox-tag.vue
@@ -0,0 +1,123 @@
+<template>
+  <view
+  :class="mainClass" :style="mainStyle"
+  >
+    <slot />
+    <iox-icon
+      v-if="closeable"
+      name="cross"
+      :custom-class="spinnerClass"
+      @click="onClose"
+    />
+  </view>
+</template>
+
+<script lang="ts">
+import Component, { mixins } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import * as utils from '../../utils/utils';
+import bem from '../../utils/bem';
+import Base from '../../mixins/base';
+
+const classPrefix = 'iox-tag';
+declare type TYpe ={
+  type?: string;
+  value?: 'default';
+} | string;
+
+@Component
+export default class IoxTag extends mixins(Base) {
+  @Prop({
+    type: String,
+    default: ''
+  })
+  size!: string;
+
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  mark!: boolean;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  color!: string;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  plain!: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  round!: boolean;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  textColor!: string;
+
+  @Prop({
+    type: [String,Object],
+    default: 'primary',
+  })
+  type!: TYpe;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  closeable!: boolean;
+
+
+
+  get utils() {
+    return utils;
+  }
+
+  get classPrefix() {
+    return classPrefix;
+  }
+
+  get mainClass() {
+    const tag: string = bem('tag', [
+      this.type,
+      this.size,
+      {
+        mark: this.mark,
+        plain: this.plain,
+        round: this.round,
+      }
+    ]);
+    return `custom-class ${this.customClass || ''} ${tag} ${this.plain ? 'iox-hairline--surround' : ''}`;
+  }
+
+  get mainStyle() {
+    return `${this.color && !this.plain ? 'background-color: '+ this.color + ';' : ''} ${this.textColor || (this.color && this.plain) ? 'color: ' + (this.textColor || this.color) + ';' : '' }`;
+  }
+
+  get spinnerClass() {
+    return `${this.classPrefix}__close`;
+  }
+
+  //hooks
+  created() {
+  }
+
+  //methods
+  onClose() {
+    this.$emit('close');
+  }
+}
+</script>
+
+<style lang="less">
+@import '../../style/widget/iox-tag/iox-tag.less';
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,70 @@
 <template>
   <view>
     hello IOX-UI!
+    <!-- button -->
     <iox-button :loading="loading" loadingType="spinner" loadingText="loading" @click="onClick" type="primary" hairline >click me!</iox-button>
     <iox-transition custom-class="block">fade!</iox-transition>
-    <iox-popup :show="popup" closeable @close="onClosePopup">Popup!</iox-popup>
+    <!-- popup -->
+    <iox-popup :show="popup" :overlay="true"
+    :round="false"
+    closeable
+    transition="slide"
+    custom-style="height: 100%;width:20%;"
+    position="left"
+    @close="onClosePopup">
+      Popup!!
+    </iox-popup>
+    <!-- tag -->
+    <block>
+
+      <iox-tag class="demo-margin-right">基础</iox-tag>
+      <iox-tag class="demo-margin-right" type="primary">标签</iox-tag>
+      <iox-tag class="demo-margin-right" type="success">标签</iox-tag>
+      <iox-tag class="demo-margin-right" type="danger">标签</iox-tag>
+      <iox-tag class="demo-margin-right" type="warning">标签</iox-tag>
+
+      <iox-tag class="demo-margin-right" round>圆角</iox-tag>
+      <iox-tag class="demo-margin-right" round type="primary">标签</iox-tag>
+      <iox-tag class="demo-margin-right" round type="success">标签</iox-tag>
+      <iox-tag class="demo-margin-right" round type="danger">标签</iox-tag>
+      <iox-tag class="demo-margin-right" round type="warning">标签</iox-tag>
+
+      <iox-tag class="demo-margin-right" mark>标记</iox-tag>
+      <iox-tag class="demo-margin-right" mark type="primary">标签</iox-tag>
+      <iox-tag class="demo-margin-right" mark type="success">标签</iox-tag>
+      <iox-tag class="demo-margin-right" mark type="danger">标签</iox-tag>
+      <iox-tag class="demo-margin-right" mark type="warning">标签</iox-tag>
+
+      <iox-tag class="demo-margin-right" plain>空心</iox-tag>
+      <iox-tag class="demo-margin-right" plain type="primary">标签</iox-tag>
+      <iox-tag class="demo-margin-right" plain type="success">标签</iox-tag>
+      <iox-tag class="demo-margin-right" plain type="danger">标签</iox-tag>
+      <iox-tag class="demo-margin-right" plain type="warning">标签</iox-tag>
+
+      <iox-tag class="demo-margin-right" color="#f2826a">自定义颜色</iox-tag>
+      <iox-tag class="demo-margin-right" color="#f2826a" plain>标签</iox-tag>
+      <iox-tag class="demo-margin-right" color="#7232dd">标签</iox-tag>
+      <iox-tag class="demo-margin-right" color="#7232dd" plain>标签</iox-tag>
+      <iox-tag class="demo-margin-right" color="#ffe1e1" text-color="#ad0000">标签</iox-tag>
+
+      <iox-tag
+        v-if="show.primary"
+        type="primary"
+        size="medium"
+        closeable
+        @close="onClose('primary')"
+        >可关闭-标签</iox-tag
+      >
+      <iox-tag
+        v-if="show.success"
+        type="success"
+        size="medium"
+        closeable
+        @close="onClose('success')"
+        >可关闭-标签</iox-tag
+      >
+    </block>
+
   </view>
 </template>
 
@@ -17,6 +78,11 @@ export default class Index extends Vue {
   loading = true;
   popup = false;
 
+  show = {
+    success: true,
+    primary: true
+  }
+
   onLoad() {
     setTimeout(() => {
       this.loading = false;
@@ -29,6 +95,11 @@ export default class Index extends Vue {
 
   onClosePopup() {
     this.popup = false;
+  }
+
+  onClose(event: TouchEvent) {
+    const dataset: any = event.currentTarget;
+    (this as any).show[dataset.id] = false;
   }
 }
 </script>
