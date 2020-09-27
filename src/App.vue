c0e546e67634b1b56f711835a7194f95ae9dd27d
Danny Zhong <lesarde@gmail.com>
2020-09-27 18:35:51 +0800

    add miniapp version upgrade check.

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default Vue.extend({
       fail: console.warn
     });
     console.log("App Launch");
+    this.checkUpdate();
   },
   onShow() {
     console.log("App Show");
@@ -33,6 +34,31 @@ export default Vue.extend({
   watch: {
   },
   methods: {
+    checkUpdate() {
+      //检查版本更新
+      const updateManager = wx.getUpdateManager();
+
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+      });
+
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            }
+          }
+        });
+      });
+
+      updateManager.onUpdateFailed(function () {
+        // 新版本下载失败
+      });
+    }
   }
 });
 </script>
