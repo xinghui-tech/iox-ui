539a79905394908b73aecc0c27e0fb3cb331bd4f
Danny Zhong <lesarde@gmail.com>
2020-09-30 21:45:57 +0800

    add contributor to demo.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,49 +11,49 @@
     </view>
     <view>
       <demo-block title="基础组件">
-        <iox-cell title="Button 按钮" url="/pages/buttons" isLink />
-        <iox-cell title="Button Group 按钮组" url="/pages/button-groups" isLink />
-        <iox-cell title="Cell 单元格" url="/pages/cells" isLink />
-        <iox-cell title="Icon 图标" url="/pages/icons" isLink />
-        <iox-cell title="Image 图片" url="/pages/images" isLink />
-        <iox-cell title="Popup 弹出层" url="/pages/popups" isLink />
-        <iox-cell title="Layout 布局" url="/pages/cols" isLink />
-        <iox-cell title="Transition 动画" url="/pages/transitions" isLink />
+        <iox-cell title="Button 按钮" url="/pages/buttons" value="老糊涂" isLink />
+        <iox-cell title="Button Group 按钮组" url="/pages/button-groups" value="老糊涂" isLink />
+        <iox-cell title="Cell 单元格" url="/pages/cells" value="老糊涂" isLink />
+        <iox-cell title="Icon 图标" url="/pages/icons" value="老糊涂" isLink />
+        <iox-cell title="Image 图片" url="/pages/images" value="老糊涂" isLink />
+        <iox-cell title="Popup 弹出层" url="/pages/popups" value="老糊涂" isLink />
+        <iox-cell title="Layout 布局" url="/pages/cols" value="老糊涂" isLink />
+        <iox-cell title="Transition 动画" url="/pages/transitions" value="老糊涂" isLink />
       </demo-block>
       <demo-block title="表单组件">
-        <iox-cell title="Calendar 日历" url="/pages/calendars" isLink />
-        <iox-cell title="Checkbox 复选框" url="/pages/checkboxs" isLink />
-        <iox-cell title="Datetime Picker 时间选择器" url="/pages/datetime-pickers" isLink />
-        <iox-cell title="Field 输入框" url="/pages/fields" isLink />
-        <iox-cell title="Picker 选择器" url="/pages/pickers" isLink />
-        <iox-cell title="Radio 单选框" url="/pages/radios" isLink />
-        <iox-cell title="Rate 评分" url="/pages/rate" isLink />
-        <iox-cell title="Slider 滑块" url="/pages/slider" isLink />
-        <iox-cell title="Search 搜索" url="/pages/searchs" isLink />
-        <iox-cell title="Stepper 步进器" url="/pages/stepper" isLink />
-        <iox-cell title="Switch 开关" url="/pages/switchs" isLink />
+        <iox-cell title="Calendar 日历" url="/pages/calendars" value="老糊涂" isLink />
+        <iox-cell title="Checkbox 复选框" url="/pages/checkboxs" value="老糊涂" isLink />
+        <iox-cell title="Datetime Picker 时间选择器" url="/pages/datetime-pickers" value="老糊涂" isLink />
+        <iox-cell title="Field 输入框" url="/pages/fields" value="老糊涂" isLink />
+        <iox-cell title="Picker 选择器" url="/pages/pickers" value="老糊涂" isLink />
+        <iox-cell title="Radio 单选框" url="/pages/radios" value="老糊涂" isLink />
+        <iox-cell title="Rate 评分" url="/pages/rate" value="春雨" isLink />
+        <iox-cell title="Slider 滑块" url="/pages/slider" value="春雨" isLink />
+        <iox-cell title="Search 搜索" url="/pages/searchs" value="春雨" isLink />
+        <iox-cell title="Stepper 步进器" url="/pages/stepper" value="春雨" isLink />
+        <iox-cell title="Switch 开关" url="/pages/switchs" value="老糊涂" isLink />
       </demo-block>
       <demo-block title="反馈组件">
-        <iox-cell title="Action-Sheet 上拉菜单" url="/pages/action-sheet" isLink />
-        <iox-cell title="Dialog 对话框" url="/pages/dialogs" isLink />
-        <iox-cell title="DropdownMenu 下拉菜单" url="/pages/dropdowns" isLink />
-        <iox-cell title="Loading 加载" url="/pages/loadings" isLink />
-        <iox-cell title="Notify 消息通知" url="/pages/notify" isLink />
-        <iox-cell title="Overlay 遮罩层" url="/pages/overlays" isLink />
-        <iox-cell title="Share Sheet 分享面板" url="/pages/share-sheet" isLink />
-        <iox-cell title="SwipeCell 滑动单元格" url="/pages/swipe-cell" isLink />
-        <iox-cell title="Toast 轻提示" url="/pages/toasts" isLink />
+        <iox-cell title="Action-Sheet 上拉菜单" url="/pages/action-sheet" value="春雨" isLink />
+        <iox-cell title="Dialog 对话框" url="/pages/dialogs" value="老糊涂" isLink />
+        <iox-cell title="DropdownMenu 下拉菜单" url="/pages/dropdowns" value="老糊涂" isLink />
+        <iox-cell title="Loading 加载" url="/pages/loadings" value="老糊涂" isLink />
+        <iox-cell title="Notify 消息通知" url="/pages/notify" value="春雨" isLink />
+        <iox-cell title="Overlay 遮罩层" url="/pages/overlays" value="老糊涂" isLink />
+        <iox-cell title="Share Sheet 分享面板" url="/pages/share-sheet" value="春雨" isLink />
+        <iox-cell title="SwipeCell 滑动单元格" url="/pages/swipe-cell" value="春雨" isLink />
+        <iox-cell title="Toast 轻提示" url="/pages/toasts" value="老糊涂" isLink />
       </demo-block>
       <demo-block title="展示组件">
-        <iox-cell title="Collapse 折叠面板" url="/pages/collapses" isLink />
-        <iox-cell title="Panel 面板" url="/pages/panels" isLink />
-        <iox-cell title="Sticky 粘性布局" url="/pages/stickies" isLink />
-        <iox-cell title="Steps 步骤条" url="/pages/steps" isLink />
-        <iox-cell title="Tag 标签" url="/pages/tags" isLink />
+        <iox-cell title="Collapse 折叠面板" url="/pages/collapses" value="老糊涂" isLink />
+        <iox-cell title="Panel 面板" url="/pages/panels" value="老糊涂" isLink />
+        <iox-cell title="Sticky 粘性布局" url="/pages/stickies" value="老糊涂" isLink />
+        <iox-cell title="Steps 步骤条" url="/pages/steps" value="春雨" isLink />
+        <iox-cell title="Tag 标签" url="/pages/tags" value="老糊涂" isLink />
       </demo-block>
       <demo-block title="导航组件">
-        <iox-cell title="Grid 宫格" url="/pages/grids" isLink />
-        <iox-cell title="Tab 标签页" url="/pages/tabs" isLink />
+        <iox-cell title="Grid 宫格" url="/pages/grids" value="老糊涂" isLink />
+        <iox-cell title="Tab 标签页" url="/pages/tabs" value="老糊涂" isLink />
       </demo-block>
     </view>
   </block>
