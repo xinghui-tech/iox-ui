9e23de0dd760ac2d4940d238627e050989770565
Danny Zhong <lesarde@gmail.com>
2020-10-03 21:11:03 +0800

    add progress

---
+++ src/components/iox-progress/iox-progress.vue
@@ -0,0 +1,95 @@
+<template>
+  <view :class="mainClass" :style="mainStyle">
+    <view
+      class="iox-progress__portion"
+      :style="'width: ' + percentage + '%; background: ' + ( inactive ? '#cacaca' : color )"
+    >
+      <view
+        v-if="showPivot && text(pivotText, percentage)"
+        :style="'color: ' +  textColor + '; background: ' + ( pivotColor ? pivotColor : inactive ? '#cacaca' : color )"
+        class="iox-progress__pivot"
+      >
+        {{ text(pivotText, percentage) }}
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import Component, { mixins } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import Base from '../../mixins/base';
+import { addUnit } from '../..//utils/utils';
+import { BLUE } from '../../utils/color';
+
+const classPrefix = 'iox-progress';
+
+@Component
+export default class IoxProgress extends mixins(Base) {
+  @Prop({
+    type: Boolean,
+  })
+  inactive?: boolean;
+
+  @Prop({
+    type: Number,
+  })
+  percentage?: number;
+
+  @Prop({
+    type: String,
+  })
+  pivotText?: string;
+
+  @Prop({
+    type: String,
+  })
+  pivotColor?: string;
+
+  @Prop({
+    type: String,
+  })
+  trackColor?: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  showPivot!: boolean;
+
+  @Prop({
+    type: String,
+    default: BLUE,
+  })
+  color!: string;
+
+  @Prop({
+    type: String,
+    default: '#fff',
+  })
+  textColor!: string;
+
+  @Prop({
+    type: [Number, String],
+    default: 4,
+  })
+  strokeWidth!: number | string;
+
+  get classPrefix() {
+    return classPrefix;
+  }
+
+  get mainStyle() {
+    return `height: ${ addUnit(this.strokeWidth) }; ${ this.trackColor ? 'background: ' + this.trackColor : '' } ${this.customStyle || ''}`;
+  }
+
+  text(pivotText: string, percentage: number) {
+    return pivotText || percentage + '%';
+  }
+}
+</script>
+
+<style lang="less">
+@import '../../style/widget/iox-progress/iox-progress.less';
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -48,6 +48,7 @@
         <iox-cell title="Collapse 折叠面板" url="/pages/collapses" :icon="iconVant" value="老糊涂" isLink />
         <iox-cell title="Noticebar 通告栏" url="/pages/noticebars" :icon="iconVant" value="老糊涂" isLink />
         <iox-cell title="Panel 面板" url="/pages/panels" :icon="iconVant" value="老糊涂" isLink />
+        <iox-cell title="Progress 进度条" url="/pages/progresses" :icon="iconVant" value="老糊涂" isLink />
         <iox-cell title="Sticky 粘性布局" url="/pages/stickies" :icon="iconVant" value="老糊涂" isLink />
         <iox-cell title="Steps 步骤条" url="/pages/steps" :icon="iconVant" value="春雨" isLink />
         <iox-cell title="Tag 标签" url="/pages/tags" :icon="iconVant" value="老糊涂" isLink />
